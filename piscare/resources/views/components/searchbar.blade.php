8a86eb2e0c07cbb7347467987b3d83a85d96a112
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2022-03-18 11:31:43 +0900

    レシピ一覧、投稿レシピ一覧の画面を作成

---
+++ piscare/resources/views/components/searchbar.blade.php
@@ -0,0 +1,46 @@
+<nav id="sidebar" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+          <div class="position-sticky pt-md-3">
+  <div class="input-group mb-3">
+  <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+  <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+</div>
+
+<p>
+  <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+    カテゴリ1
+  </button>
+</p>
+<div class="collapse" id="collapseExample">
+  <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="">カテゴリ1-1</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">カテゴリ1-2</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">カテゴリ1-3</a>
+                </li>
+            </ul>
+</div>
+
+<p>
+  <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample1" aria-expanded="false" aria-controls="collapseExample">
+    カテゴリ2
+  </button>
+</p>
+<div class="collapse" id="collapseExample1">
+  <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="">カテゴリ2-1</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">カテゴリ2-2</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">カテゴリ2-3</a>
+                </li>
+            </ul>
+</div>
+</div>
+      </nav>
