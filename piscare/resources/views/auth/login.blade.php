58848bd081d533770ddea9d12b0dd08c3aad31b1
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2022-04-30 21:10:41 +0900

    auth関連のレイアウトの変更

--- piscare/resources/views/auth/login.blade.php
+++ piscare/resources/views/auth/login.blade.php
@@ -1,13 +1,16 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
-@section('content')
+@section('header')
+    @include('components.header.auth', ['page' => 'login'])
+@endsection
+
+@section('main')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
+            <div class="card auth-card">
+                <div class="card-body auth-card-body">
+                    <h2 class="auth-title">Login</h2>
                     @if ($errors->any())
                         <div class="alert alert-danger">
                             <ul class="mb-0 mt-0">
@@ -17,11 +20,11 @@
                             </ul>
                         </div>
                     @endif
-                    <form method="POST" action="{{ route('login') }}">
+                    <form method="POST" action="{{ route('login') }}" class="aurh-form">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                        <div class="form-group row auth-form-group">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">メールアドレス</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -34,8 +37,8 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                        <div class="form-group row auth-form-group">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">パスワード</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -48,7 +51,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="form-group row auth-form-group">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
@@ -60,33 +63,33 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                        <div class="form-group row auth-form-group">
+                            <div class="col-md-8  d-grid mx-auto">
+                                <button type="submit" class="btn auth-button">
+                                    Login
                                 </button>
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                        パスワードを忘れた方はこちら
                                     </a>
                                 @endif
                             </div>
                         </div>
                     </form>
-                    <div class="d-grid gap-2 col-8 mx-auto">
-                        <a href="{{ route('login.{provider}', ['provider' => 'google']) }}" class="btn btn-block btn-danger">
-                            <i class="fab fa-google mr-1"></i>Googleでログイン
+                    <div class="d-grid gap-2 col-8 mx-auto auth-button-social">
+                        <a href="{{ route('login.{provider}', ['provider' => 'google']) }}" class="btn btn-block auth-button-google auth-button-social-link">
+                            <i class="fab fa-google auth-button-social-icon"></i>Googleでログイン
                         </a>
                     </div>
-                    <div class="d-grid gap-2 col-8 mx-auto">
-                        <a href="{{ route('login.{provider}', ['provider' => 'github']) }}" class="btn btn-block btn-secondary">
-                            <i class="fab fa-github mr-1"></i>GitHubでログイン
+                    <div class="d-grid gap-2 col-8 mx-auto auth-button-social">
+                        <a href="{{ route('login.{provider}', ['provider' => 'github']) }}" class="btn btn-block auth-button-github auth-button-social-link">
+                            <i class="fab fa-github auth-button-social-icon"></i>GitHubでログイン
                         </a>
                     </div>
-                    <div class="d-grid gap-2 col-8 mx-auto">
-                        <a href="{{ route('login.{provider}', ['provider' => 'twitter']) }}" class="btn btn-block btn-primary">
-                            <i class="fab fa-twitter mr-1"></i>Twitterでログイン
+                    <div class="d-grid gap-2 col-8 mx-auto auth-button-social">
+                        <a href="{{ route('login.{provider}', ['provider' => 'twitter']) }}" class="btn btn-block auth-button-twitter auth-button-social-link">
+                            <i class="fab fa-twitter auth-button-social-icon"></i>Twitterでログイン
                         </a>
                     </div>
                 </div>
--- piscare/resources/views/auth/passwords/email.blade.php
+++ piscare/resources/views/auth/passwords/email.blade.php
@@ -1,24 +1,28 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
-@section('content')
+@section('header')
+    @include('components.header.auth', ['page' => 'email'])
+@endsection
+
+@section('main')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+            <div class="card auth-card">
 
-                <div class="card-body">
+                <div class="card-body auth-card-body">
+                    <h2 class="auth-title">Reset Password</h2>
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
 
-                    <form method="POST" action="{{ route('password.email') }}">
+                    <form method="POST" action="{{ route('password.email') }}" class="aurh-form">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                        <div class="form-group row auth-form-group">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">メールアドレス</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -31,10 +35,10 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
+                        <div class="form-group row auth-form-group">
+                            <div class="col-md-8  d-grid mx-auto">
+                                <button type="submit" class="btn auth-button">
+                                    パスワード再設定リンクを送信
                                 </button>
                             </div>
                         </div>
