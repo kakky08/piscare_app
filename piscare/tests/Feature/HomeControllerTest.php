bba8859d13e7819b8db7b6095b27dc41d17226fb
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2022-06-01 23:17:05 +0900

    テストの追加

---
+++ piscare/tests/Feature/HomeControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HomeControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testIndex()
+    {
+        $response = $this->get(route('home.index'));
+
+        $response->assertStatus(200)->assertViewIs('mypage.home.home');
+    }
+}
