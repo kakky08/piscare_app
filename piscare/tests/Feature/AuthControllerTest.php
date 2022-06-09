4c88dca5a8d293654ae279ab7811325ed9bd4475
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2022-06-09 09:21:38 +0900

    テストの変更

--- piscare/tests/Feature/AuthControllerTest.php
+++ piscare/tests/Feature/AuthControllerTest.php
@@ -5,20 +5,23 @@ namespace Tests\Feature;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use SebastianBergmann\Environment\Console;
 use Tests\TestCase;
 
-class HomeControllerTest extends TestCase
+class AuthControllerTest extends TestCase
 {
-    // use RefreshDatabase;
-
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
     public function testIndex()
     {
         $user = factory(User::class)->create();
 
         $response = $this->actingAs($user)
-        ->get(route('home.index'));
+            ->get(route('home.index'));
 
         $response->assertStatus(200)->assertViewIs('mypage.home.home');
+
     }
 }
